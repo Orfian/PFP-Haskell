3db190ed5ccb830e94219d457f62514dd0c7f588
Orfian <orfian963@gmail.com>
2024-10-22 11:21:28 +0200

    ..

--- 2nd.hs
+++ 2nd.hs
@@ -37,12 +37,69 @@ regIter :: RegExpr
 regIter = Iteration (Symbol 'a')
 
 
+isDeterministic :: Automaton -> Bool
+isDeterministic (n, sigma, delta, q0, f) = all checkState [0..n-1]
+    where
+        -- check all symbols for each state q
+        checkState q = all (checkSymbol q) sigma
+        -- check if there is exactly one transition for each state q and symbol a
+        checkSymbol q a = length (filter (matchesTransition q a) delta) == 1
+        -- check if transition starts in state q with symbol a
+        matchesTransition q a (q1, a1, q2) = q1 == q && a1 == a
+
+
 -- Convert a regular expression to DFA
 convert2 :: RegExpr -> Automaton
-convert2 r = convertNfaDfa (convertRegNfa r)
+convert2 r = convert (convertRegNfa r)
 
-convertNfaDfa :: Automaton -> Automaton
-convertNfaDfa (nStates, alphabet, transitions, start, acceptStates) = (nStates, alphabet, transitions, start, acceptStates)
+-- Epsilon closure helper function
+epsilonClosure :: [Transition] -> [Int] -> [Int]
+epsilonClosure delta states =
+    let step closure = nub (closure ++ [q2 | q1 <- closure, (q1', 'e', q2) <- delta, q1 == q1'])
+    in until (\s -> sort (step s) == sort s) step states
+
+-- The main conversion function
+convert :: Automaton -> Automaton
+convert (n, sigma, delta, q0, f)
+    | isDeterministic (n, sigma, delta, q0, f) = (n, sigma, delta, q0, f)
+    | otherwise = convertInner (n, sigma, delta, q0, f)
+
+-- Core DFA conversion with epsilon closure
+convertInner :: Automaton -> Automaton
+convertInner (n, sigma, delta, q0, f) = (n', sigma, dfaDelta, 0, dfaFinals)
+    where
+        -- Initial state is the epsilon closure of the NFA's start state
+        initialState = epsilonClosure delta [q0]
+        
+        -- State sets in DFA correspond to subsets of NFA states (including epsilon closure)
+        dfaStates = generateDFAStates [initialState] [] sigma delta
+        dfaStateIndex = zip dfaStates [0..]
+        n' = length dfaStates
+
+        -- Transitions for the DFA
+        dfaDelta = [(dfaStateIndexLookup src, a, dfaStateIndexLookup dest)
+                    | src <- dfaStates,
+                      a <- sigma,
+                      let dest = sort $ nub (epsilonClosure delta [q2 | q1 <- src, (q1', a', q2) <- delta, q1 == q1' && a == a'])]
+
+        -- DFA final states
+        dfaFinals = [j | (qs, j) <- dfaStateIndex, any (`elem` f) qs]
+
+        -- Lookup DFA state index for a given set of NFA states
+        dfaStateIndexLookup qs = case filter ((== qs) . fst) dfaStateIndex of
+                                   [] -> error ("State set not found: " ++ show qs)
+                                   ((_, j):_) -> j
+
+-- Recursive function to generate DFA states
+generateDFAStates :: [[Int]] -> [[Int]] -> String -> [Transition] -> [[Int]]
+generateDFAStates [] visited _ _ = visited
+generateDFAStates (current:rest) visited sigma delta
+    | current `elem` visited = generateDFAStates rest visited sigma delta
+    | otherwise =
+        let newStates = [sort $ nub (epsilonClosure delta [q2 | q1 <- current, (q1', a, q2) <- delta, q1 == q1', a == a'])
+                         | a <- sigma]
+            newUnvisited = filter (`notElem` visited) newStates
+        in generateDFAStates (rest ++ newUnvisited) (visited ++ [current]) sigma delta
 
 convertRegNfa :: RegExpr -> Automaton
 convertRegNfa Epsilon = (1, "", [], 0, [0])
